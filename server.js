fdce35a84ee7cf040dbc7d841f5355dc52fc3b69
Qasimsaifi <106295428+Qasimsaifi@users.noreply.github.com>
2023-08-11 15:56:41 +0530

    done

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const errorHandler = require("./middleware/errorhandler");
 const connectDb = require("./config/dbConnection");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
-
+const path = require("path");
 connectDb();
 
 const app = express();
@@ -17,6 +17,8 @@ app.use(cors());
 
 app.use("/api/contacts", require("./routes/contactsRouter"));
 app.use("/api/users", require("./routes/userRouter"));
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(errorHandler);
 
